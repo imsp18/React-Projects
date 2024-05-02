63d58c4e9f072006514d37fe38e0f8945e01aa8c
imsp18 <shivangparmar537@gmail.com>
2024-05-02 14:26:03 +0530

    made the accordion, which on click expands and collapses

--- 1_accordion/src/App.js
+++ 1_accordion/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [button, setButton] = useState(false);
+
+  function handleClick() {
+    setButton(!button);
+    console.log(button);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex flex-col gap-10 m-10 justify-center items-center">
+        <h1 className="flex text-3xl font-serif">Accordion Project</h1>
+        <div className="p-2 gap-2 flex flex-col bg-gray-400 font-serif rounded-md w-full">
+          <div className="items-center gap-2 flex justify-between">
+            <label className="font-bold">
+              Is this Hackathon Free to participate ?
+            </label>
+            <button
+              className="px-2 rounded-sm text-gray-200 bg-gray-900 "
+              onClick={handleClick}
+            >
+              click
+            </button>
+          </div>
+          {button ? (
+            <div>
+              <div className="flex-1 w-full border-black  border-t-[1px]"></div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                ac lacus a risus vestibulum placerat. Curabitur malesuada
+                consequat sollicitudin. Donec in dolor sed massa pretium
+                vestibulum id ut odio. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Cras vel arcu non lacus sollicitudin finibus.
+              </p>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
