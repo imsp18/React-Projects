bdb33fded5f1a1562099dcef5461dbdcf626869a
Shivang Parmar <shivangparmar537@gmail.com>
2024-05-17 21:46:31 +0530

    refactor: Improve image slider functionality and styles

--- 6_image_slider/src/App.jsx
+++ 6_image_slider/src/App.jsx
@@ -27,6 +27,14 @@ export default function App() {
     }
   }
 
+  function handlePrevious() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1)
+  }
+
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1)
+  }
+
   useEffect(() => {
     if (url !== '') fetchImages(url)
   }, [url])
@@ -42,29 +50,32 @@ export default function App() {
   }
 
   return <div className="container">
-    <BsArrowLeftCircleFill className="arrow arrow-left" />
+    <BsArrowLeftCircleFill onClick={handlePrevious} className="arrow arrow-left" />
     {
       images && images.length ?
-        images.map(imageItem => (
+        images.map((imageItem, index) => (
           <img
             key={imageItem.id}
             alt={imageItem.download_url}
             src={imageItem.download_url}
-            className="current-image"
+            className={currentSlide === index ? "current-image" : "current-image hide-current-image"}
           />
         ))
         : null}
-        <BsArrowRightCircleFill className="arrow arrow-right" />
-        <span className="circle-indicators">
-          {
-            images && images.length ?
-            images.map((_,index) => <button
-            key={index}
-            className="current-indicator">
-              
-            </button>) :
-            null
+    <BsArrowRightCircleFill onClick={handleNext} className="arrow arrow-right" />
+    <span className="circle-indicators">
+      {
+        images && images.length ?
+          images.map((_, index) => <button className={
+            currentSlide === index ? "current-indicator" : "current-indicator hide-current-indicator"
           }
-        </span>
+          onClick={() => setCurrentSlide(index)}
+            key={index}
+          >
+
+          </button>) :
+          null
+      }
+    </span>
   </div>
 }
