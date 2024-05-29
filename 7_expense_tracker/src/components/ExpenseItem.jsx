398062dfc1ba019738e01bc780ed1a000da208d8
Shivang Parmar <shivangparmar537@gmail.com>
2024-05-29 16:24:19 +0530

    refactor: Update ExpenseItem component to display amount in Indian Rupees (₹)

--- 7_expense_tracker/src/components/ExpenseItem.jsx
+++ 7_expense_tracker/src/components/ExpenseItem.jsx
@@ -2,7 +2,7 @@ export default function ExpenseItem({ expense }) {
     return (
         <div className="flex justify-between bg-white p-4 shadow mb-2 rounded">
             <div>{expense.name}</div>
-            <div className="text-red-500">${expense.amount}</div>
+            <div className="text-red-500">₹ {expense.amount}</div>
         </div>
     )
 }
