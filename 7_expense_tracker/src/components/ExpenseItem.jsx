40d84daf73f3e8dd98f459d817372f04e7f8a934
Shivang Parmar <shivangparmar537@gmail.com>
2024-05-29 10:51:40 +0530

    feat: Add ExpenseItem component to display individual expenses

---
+++ 7_expense_tracker/src/components/ExpenseItem.jsx
@@ -0,0 +1,8 @@
+export default function ExpenseItem({ expense }) {
+    return (
+        <div className="flex justify-between bg-white p-4 shadow mb-2 rounded">
+            <div>{expense.name}</div>
+            <div className="text-red-500">${expense.amount}</div>
+        </div>
+    )
+}
