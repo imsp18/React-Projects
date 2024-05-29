063919d83ab9151c0157b1a41c5973d10db2ddb5
Shivang Parmar <shivangparmar537@gmail.com>
2024-05-29 16:24:28 +0530

    refactor: Improve AddExpenseForm validation and error handling

--- 7_expense_tracker/src/components/AddExpenseForm.jsx
+++ 7_expense_tracker/src/components/AddExpenseForm.jsx
@@ -7,23 +7,30 @@ export default function AddExpenseForm({ onAddExpense }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onAddExpense({ name, amount: parseFloat(amount), id: Date.now() });
-    setName('');
-    setAmount('');
+    if (name && amount > 0) {
+      onAddExpense({ name, amount: parseFloat(amount), id: Date.now() });
+      setName('');
+      setAmount('');
+    }
+    else {
+      alert("Please enter a valid expense name and amount. ");
+    }
+
   };
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col  bg-white p-4 shadow-lg rounded mb-4">
       <div className='flex gap-4'>
         <div className="mb-2">
-        <label className="block text-sm">Expense Name</label>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="border p-2 w-full"
-        />
-      </div>
+          <label className="block text-sm">Expense Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="border p-2 w-full"
+            placeholder="Enter expense name"
+          />
+        </div>
         <div className="mb-2">
           <label className="block text-sm">Amount</label>
           <input
@@ -31,6 +38,7 @@ export default function AddExpenseForm({ onAddExpense }) {
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
             className="border p-2 w-full"
+            placeholder="Enter amount"
           />
         </div></div>
       <button type="submit" className="bg-blue-500 text-white p-2 rounded">
