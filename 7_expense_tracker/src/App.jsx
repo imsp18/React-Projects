6d98ffaf8f9bb9e49658fee88d11bb8bf7abb399
Shivang Parmar <shivangparmar537@gmail.com>
2024-05-29 10:53:12 +0530

    refactor: Update App component to include Header, AddExpenseForm, and ExpenseList components

--- 7_expense_tracker/src/App.jsx
+++ 7_expense_tracker/src/App.jsx
@@ -1,7 +1,23 @@
+// src/App.jsx
+import { useState } from 'react';
+import Header from './components/Header';
+import ExpenseList from './components/ExpenseList';
+import AddExpenseForm from './components/AddExpenseForm';
+
 export default function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  const handleAddExpense = (expense) => {
+    setExpenses([...expenses, expense]);
+  };
+
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow container mx-auto p-4">
+        <AddExpenseForm onAddExpense={handleAddExpense} />
+        <ExpenseList expenses={expenses} />
+      </main>
+    </div>
+  );
 }
