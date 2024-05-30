1e3ce066cbd7dd7e06835174a890051cf5c58ba5
Shivang Parmar <shivangparmar537@gmail.com>
2024-05-30 12:42:24 +0530

    refactor: Update AddExpenseForm component to include payment method selection

--- 7_expense_tracker/src/components/AddExpenseForm.jsx
+++ 7_expense_tracker/src/components/AddExpenseForm.jsx
@@ -4,13 +4,15 @@ import { useState } from 'react';
 export default function AddExpenseForm({ onAddExpense }) {
   const [name, setName] = useState('');
   const [amount, setAmount] = useState('');
+  const [paymentMethod, setPaymentMethod] = useState('cash'); //default state is set to cash
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name && amount > 0) {
-      onAddExpense({ name, amount: parseFloat(amount), id: Date.now() });
+      onAddExpense({ name, amount: parseFloat(amount), paymentMethod, id: Date.now() });
       setName('');
       setAmount('');
+      setPaymentMethod('cash'); //default
     }
     else {
       alert("Please enter a valid expense name and amount. ");
@@ -19,31 +21,63 @@ export default function AddExpenseForm({ onAddExpense }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col  bg-white p-4 shadow-lg rounded mb-4">
-      <div className='flex gap-4'>
-        <div className="mb-2">
-          <label className="block text-sm">Expense Name</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="border p-2 w-full"
-            placeholder="Enter expense name"
-          />
+    <div>
+      <div className="text-3xl px-4">Add Your Expenses</div>
+      <form onSubmit={handleSubmit} className="flex flex-col m-4  bg-gray-200 border border-gray-500 rounded-lg p-4 shadow-lg  mb-4">
+
+        <div className='flex-col gap-4'>
+          <div className='flex gap-4'>
+            <div className="mb-2">
+              <label className="block text-md p-1 font-semibold">Expense Name:</label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="border p-2 w-full bg-transparent border-black rounded-md"
+                placeholder="Enter expense name"
+              />
+            </div>
+            <div className="mb-2">
+              <label className="block text-md p-1 font-semibold">Amount:</label>
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                className="border p-2 w-full bg-transparent border-black rounded-md"
+                placeholder="Enter amount"
+              />
+            </div>
+          </div>
+          <div className="mb-2 ">
+            <label className='block text-md p-1 font-semibold'>Payment Method:</label>
+            <div className="flex items-center gap-4">
+              <label className='flex items-center'>
+                <input
+                  type="radio"
+                  value="cash"
+                  checked={paymentMethod === 'cash'}
+                  onChange={() => setPaymentMethod('cash')}
+                  className="mr-2"
+                />
+                Cash
+              </label>
+              <label className='flex items-center'>
+                <input
+                  type="radio"
+                  value="upi"
+                  checked={paymentMethod === 'upi'}
+                  onChange={() => setPaymentMethod('upi')}
+                  className="mr-2"
+                />
+                UPI
+              </label>
+            </div>
+          </div>
         </div>
-        <div className="mb-2">
-          <label className="block text-sm">Amount</label>
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            className="border p-2 w-full"
-            placeholder="Enter amount"
-          />
-        </div></div>
-      <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-        Add Expense
-      </button>
-    </form>
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+          Add Expense
+        </button>
+      </form>
+    </div>
   );
 }
--- 7_expense_tracker/src/components/ExpenseItem.jsx
+++ 7_expense_tracker/src/components/ExpenseItem.jsx
@@ -5,12 +5,18 @@ export default function ExpenseItem({ expense, onRemove }) {
     };
 
     return (
-        <div className="flex items-center justify-between bg-white p-4 shadow mb-2 rounded">
-            
-            <div>{expense.name}</div>
+        <div className="flex items-center justify-between bg-gray-200 border m-4 border-gray-500 rounded-lg p-4 shadow-lg mb-2">
+
+            <div className="flex-col items-start justify-between">
+                <div>{expense.name}</div>
+                {expense.paymentMethod === 'cash' ?
+                    <span className="bg-green-100 text-green-800 text-[10px] font-medium me-2 p-1 rounded dark:bg-green-900 dark:text-green-300">Cash Payment</span> : <span className="bg-blue-100 text-blue-800 text-[10px] font-medium me-2 p-1 rounded dark:bg-blue-900 dark:text-blue-300">UPI Payment</span>}
+            </div>
             <div className="flex gap-3 items-center">
                 <div className="text-blue-500">₹ {expense.amount}</div>
-                <button onClick={handleRemove} className="text-sm px-2 py-1 bg-red-300 rounded-sm text-red-600 font-semibold border border-red-600">Remove</button>
+                <button onClick={handleRemove} className="text-sm bg-red-300 rounded-sm text-red-600 font-semibold border border-red-600"><svg className="h-3 w-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                </svg></button>
             </div>
         </div>
     )
