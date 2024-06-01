0100a8cc56e37cbdc51d1e9d3b2d530a3a612061
Shivang Parmar <shivangparmar537@gmail.com>
2024-06-01 18:22:57 +0530

    refactor: Update password generator logic and UI

--- 8_Password_Generator/src/App.jsx
+++ 8_Password_Generator/src/App.jsx
@@ -6,19 +6,19 @@ function App() {
   const [character, setCharacter] = useState(false);
   const [password, setPassword] = useState("");
 
-  const passwordGenerator = useCallback(()=>{
+  const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(number) str += "0123456789"
-    if(character) str += "!@#$%^&*-_=+[]{}~`"
+    if (number) str += "0123456789"
+    if (character) str += "!@#$%^&*-_=+[]{}~`"
 
-    for(let i = 1; i<= array.length; i++){
+    for (let i = 1; i <= array.length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
       pass += char;
     }
     setPassword(pass)
 
-  },  [length, number, character, setPassword])
+  }, [length, number, character, setPassword])
 
   return (
     <>
@@ -27,6 +27,27 @@ function App() {
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly />
           <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className="flex items-center gap-x-1">
+            <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={(e) => { setLength(e.target.value) }} />
+            <label>Length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox"
+              defaultChecked={number}
+              id='numberInput'
+              onChange={() => setNumber((prev) => !prev)} />
+            <label htmlFor='numberInput'>Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox"
+              defaultChecked={character}
+              id='characterInput'
+              onChange={() => setCharacter((prev) => !prev)} />
+            <label htmlFor='numberInput'>Characters</label>
+          </div>
+
         </div>
       </div>
     </>
