959bee6eea64d578f5839fdeb7880290230efff4
Shivang Parmar <shivangparmar537@gmail.com>
2024-06-02 21:52:35 +0530

    refactor: Improve password generator logic and UI

--- 8_Password_Generator/src/App.jsx
+++ 8_Password_Generator/src/App.jsx
@@ -1,10 +1,20 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState, useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8);
   const [number, setNumber] = useState(false);
   const [character, setCharacter] = useState(false);
   const [password, setPassword] = useState("");
+
+  //useRef hook
+  const passwordRef = useRef(null);
+
+  const copyPasswordToClip = useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0, 100)
+
+    window.navigator.clipboard.writeText(password);
+  }, [password])
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -28,8 +38,10 @@ function App() {
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly />
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+          <input type="text" value={password} 
+          ref={passwordRef}
+          className='outline-none w-full py-1 px-3' placeholder='password' readOnly />
+          <button onClick={copyPasswordToClip} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className="flex items-center gap-x-1">
